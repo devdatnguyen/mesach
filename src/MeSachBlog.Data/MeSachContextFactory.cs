b2639c2c48da445fcb8abc030de4eed14f7883d0
Dat Nguyen <95663983+devdatnguyen@users.noreply.github.com>
2024-11-29 11:16:42 +0700

    Migration Init 2.7

---
+++ src/MeSachBlog.Data/MeSachContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeSachBlog.Data
+{
+    public class MeSachContextFactory : IDesignTimeDbContextFactory<MeSachContext>
+    {
+        public MeSachContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<MeSachContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new MeSachContext(builder.Options);
+        }
+    }
+}
